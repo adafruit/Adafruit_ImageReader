8c35e9fbc4a8ff3c41d241a41f5d079c3769b2b0
Travis CI <travis@travis-ci.org>
2019-11-22 20:41:13 +0000

    Deploy code docs to GitHub Pages Travis build: 113
    Commit: ec73146dd43c83d61e12cb6710a0f000416f2b41

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['dealloc',['dealloc',['../class_adafruit___image.html#a775ca41829f41398bea9e719d0297e34',1,'Adafruit_Image']]],
-  ['draw',['draw',['../class_adafruit___image.html#a96f77402b729e22bf375ec07116d91cd',1,'Adafruit_Image']]],
+  ['draw',['draw',['../class_adafruit___image.html#a96f77402b729e22bf375ec07116d91cd',1,'Adafruit_Image::draw(Adafruit_SPITFT &amp;tft, int16_t x, int16_t y)'],['../class_adafruit___image.html#aacd7e026e3e8092c6f59dcb0d715950a',1,'Adafruit_Image::draw(Adafruit_EPD &amp;epd, int16_t x, int16_t y)']]],
   ['drawbmp',['drawBMP',['../class_adafruit___image_reader.html#a4a033f474148e90a7a5e755f3531e401',1,'Adafruit_ImageReader']]]
 ];
