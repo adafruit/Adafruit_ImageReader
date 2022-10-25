90871c298f0dcb43439c954796a74a76abc6ae0d
hathach <thach@tinyusb.org>
2022-10-25 15:17:27 +0700

    update SdFat v2 compatible
    - use FatVolume to replace FatFileSystem
    - use File32 to replace File

--- Adafruit_ImageReader.h
+++ Adafruit_ImageReader.h
@@ -103,8 +103,8 @@ public:
   void printStatus(ImageReturnCode stat, Stream &stream = Serial);
 
 protected:
-  FatFileSystem *filesys; ///< FAT FileSystem Object
-  File file;              ///< Current Open file
+  FatVolume *filesys; ///< FAT FileSystem Object
+  File32 file;              ///< Current Open file
   ImageReturnCode coreBMP(const char *filename, Adafruit_SPITFT *tft,
                           uint16_t *dest, int16_t x, int16_t y,
                           Adafruit_Image *img, boolean transact);
