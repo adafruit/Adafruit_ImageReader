04b3d3f7b750a778bb0b51f9b14d89ab68ade1b6
Liz <liz@adafruit.com>
2025-08-01 09:27:48 -0400

    clang again

--- Adafruit_ImageReader.h
+++ Adafruit_ImageReader.h
@@ -39,42 +39,48 @@ enum ImageFormat {
            ImageReader.loadBMP() and Image.draw(), not ImageReader.drawBMP().
 */
 class Adafruit_Image {
-public:
+ public:
   Adafruit_Image(void);
   ~Adafruit_Image(void);
   int16_t width(void) const;  // Return image width in pixels
   int16_t height(void) const; // Return image height in pixels
-  void draw(Adafruit_SPITFT &tft, int16_t x, int16_t y);
+  void draw(Adafruit_SPITFT& tft, int16_t x, int16_t y);
   /*!
       @brief   Return canvas image format.
       @return  An ImageFormat type: IMAGE_1 for a GFXcanvas1, IMAGE_8 for
                a GFXcanvas8, IMAGE_16 for a GFXcanvas16, IMAGE_NONE if no
                canvas currently allocated.
   */
-  ImageFormat getFormat(void) const { return (ImageFormat)format; }
-  void *getCanvas(void) const;
+  ImageFormat getFormat(void) const {
+    return (ImageFormat)format;
+  }
+  void* getCanvas(void) const;
   /*!
       @brief   Return pointer to color palette.
       @return  Pointer to an array of 16-bit color values, or NULL if no
                palette associated with image.
   */
-  uint16_t *getPalette(void) const { return palette; }
+  uint16_t* getPalette(void) const {
+    return palette;
+  }
   /*!
       @brief   Return pointer to 1bpp image mask canvas.
       @return  GFXcanvas1* pointer (1-bit RAM-resident image) if present,
                NULL otherwise.
   */
-  GFXcanvas1 *getMask(void) const { return mask; };
+  GFXcanvas1* getMask(void) const {
+    return mask;
+  };
 
-protected:
+ protected:
   // MOST OF THESE ARE NOT SUPPORTED YET -- WIP
   union {                  // Single pointer, only one variant is used:
-    GFXcanvas1 *canvas1;   ///< Canvas object if 1bpp format
-    GFXcanvas8 *canvas8;   ///< Canvas object if 8bpp format
-    GFXcanvas16 *canvas16; ///< Canvas object if 16bpp
+    GFXcanvas1* canvas1;   ///< Canvas object if 1bpp format
+    GFXcanvas8* canvas8;   ///< Canvas object if 8bpp format
+    GFXcanvas16* canvas16; ///< Canvas object if 16bpp
   } canvas;                ///< Union of different GFXcanvas types
-  GFXcanvas1 *mask;        ///< 1bpp image mask (or NULL)
-  uint16_t *palette;       ///< Color palette for 8bpp image (or NULL)
+  GFXcanvas1* mask;        ///< 1bpp image mask (or NULL)
+  uint16_t* palette;       ///< Color palette for 8bpp image (or NULL)
   uint8_t format;          ///< Canvas bundle type in use
   void dealloc(void);      ///< Free/deinitialize variables
   friend class Adafruit_ImageReader; ///< Loading occurs here
@@ -93,21 +99,21 @@ protected:
            for use.
 */
 class Adafruit_ImageReader {
-public:
-  Adafruit_ImageReader(FatVolume &fs);
+ public:
+  Adafruit_ImageReader(FatVolume& fs);
   ~Adafruit_ImageReader(void);
-  ImageReturnCode drawBMP(const char *filename, Adafruit_SPITFT &tft, int16_t x,
+  ImageReturnCode drawBMP(const char* filename, Adafruit_SPITFT& tft, int16_t x,
                           int16_t y, boolean transact = true);
-  ImageReturnCode loadBMP(const char *filename, Adafruit_Image &img);
-  ImageReturnCode bmpDimensions(const char *filename, int32_t *w, int32_t *h);
-  void printStatus(ImageReturnCode stat, Stream &stream = Serial);
+  ImageReturnCode loadBMP(const char* filename, Adafruit_Image& img);
+  ImageReturnCode bmpDimensions(const char* filename, int32_t* w, int32_t* h);
+  void printStatus(ImageReturnCode stat, Stream& stream = Serial);
 
-protected:
-  FatVolume *filesys; ///< FAT FileSystem Object
+ protected:
+  FatVolume* filesys; ///< FAT FileSystem Object
   File32 file;        ///< Current Open file
-  ImageReturnCode coreBMP(const char *filename, Adafruit_SPITFT *tft,
-                          uint16_t *dest, int16_t x, int16_t y,
-                          Adafruit_Image *img, boolean transact);
+  ImageReturnCode coreBMP(const char* filename, Adafruit_SPITFT* tft,
+                          uint16_t* dest, int16_t x, int16_t y,
+                          Adafruit_Image* img, boolean transact);
   uint16_t readLE16(void);
   uint32_t readLE32(void);
 };
