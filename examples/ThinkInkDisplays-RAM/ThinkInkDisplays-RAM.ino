7d941a93917170d7916aab275986c80c3a27d50a
brentru <brent@adafruit.com>
2026-07-07 12:09:34 -0400

    Add blinka instead

--- examples/ThinkInkDisplays-RAM/ThinkInkDisplays-RAM.ino
+++ examples/ThinkInkDisplays-RAM/ThinkInkDisplays-RAM.ino
@@ -4,7 +4,7 @@
 #include <Adafruit_GFX.h>         // Core graphics library
 #include "Adafruit_ThinkInk.h"
 #include <Adafruit_ImageReader_EPD.h> // Image-reading functions
-#include "adabot_head.h"
+#include "blinka.h"
 
 
 // Mono Displays
@@ -38,7 +38,6 @@ ThinkInk_290_Grayscale4_EAAMFGN display(EPD_DC, EPD_RESET, EPD_CS, -1, -1);
 // Reader (no filesystem, in-memory only)
 Adafruit_ImageReader_EPD reader;
 
-Adafruit_Image_EPD   img;        // An image loaded into RAM
 int32_t              width  = 0, // BMP image dimensions
                      height = 0;
 
@@ -48,14 +47,14 @@ void setup(void) {
   display.begin(THINKINK_GRAYSCALE4);
   display.clearBuffer();
 
-  ImageReturnCode rc = reader.drawBMP(adabot_head_bmp, ADABOT_HEAD_BMP_LEN, display, 0, 0);
+  ImageReturnCode rc = reader.drawBMP(blinka_bmp, BLINKA_BMP_LEN, display, 0, 0);
   Serial.print("drawBMP rc = ");
   Serial.println(rc);          // 0 == IMAGE_SUCCESS
   display.display();
 
-  // Query the dimensions of image 'adabot_head_bmp' WITHOUT loading to screen:
-  Serial.print(F("Querying adabot_head_bmp image size..."));
-  stat = reader.bmpDimensions(adabot_head_bmp, &width, &height);
+  // Query the dimensions of image 'blinka_bmp' WITHOUT loading to screen:
+  Serial.print(F("Querying blinka_bmp image size..."));
+  ImageReturnCode stat = reader.bmpDimensions(blinka_bmp, &width, &height);
   reader.printStatus(stat);   // How'd we do?
   if(stat == IMAGE_SUCCESS) { // If it worked, print image size...
     Serial.print(F("Image dimensions: "));
@@ -63,7 +62,6 @@ void setup(void) {
     Serial.write('x');
     Serial.println(height);
   }
-
 }
 
 void loop() {
@@ -71,7 +69,7 @@ void loop() {
     display.setRotation(r);    // Set rotation
     display.fillScreen(0);     // and clear screen
     display.clearBuffer();
-    reader.drawBMP(adabot_head_bmp, ADABOT_HEAD_BMP_LEN, display, 0, 0);
+    reader.drawBMP(blinka_bmp, BLINKA_BMP_LEN, display, 0, 0);
     display.display();
     delay(30 * 1000); // Pause 30 sec.
   }
